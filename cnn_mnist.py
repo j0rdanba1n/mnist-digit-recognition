9300cde8ee036dfce735bd352ee5a1f87a6b26fa
Jordan Bain <bain3@wisc.edu>
2018-03-26 15:59:16 -0500

    Create Estimator To run training and evaluation on data set

--- cnn_mnist.py
+++ cnn_mnist.py
@@ -21,6 +21,37 @@ def main(unused_argv):
     eval_data = mnist.test.images # Returns np.array
     eval_labels = np.asarray(mnist.test.labels, dtype=np.int32)
 
+    # Create the Estimator
+    mnist_classifier = tf.estimator.Estimator(model_fn = cnn_model_fn, model_dir = "/tmp/mnist_convnet_model")
+
+    # Set up logging for predictions
+    tensors_to_log = {"probabilities" : "softmax_tensor"}
+    logging_hook = tf.train.LoggingTensorHook(tensors = tensors_to_log, every_n_iter = 50)
+
+    # Train the model
+    train_input_fn = tf.estimator.inputs.numpy_input_fn
+    (
+        x = {"x" : train_data},
+        y = train_labels,
+        batch_size = 100,
+        num_epochs = None,
+        shuffle = True
+    )
+
+    mnist_classifier.train(input_fn = train_input_fn, steps = 20000, hooks=[logging_hook])
+
+    # Evaluate the model and print results
+    eval_input_fn = tf.estimator.inputs.numpy_input_fn
+    (
+        x = {"x" : eval_data},
+        y = eval_labels,
+        num_epochs = 1,
+        shuffle = False
+    )
+
+    eval_results = mnist_classifier.evaluate(input_fn = eval_input_fn)
+    print (eval_results)
+
 def cnn_model_fn (features, labels, mode):
     """Model function for Convolutional Neural Networks"""
     # Input Layer
